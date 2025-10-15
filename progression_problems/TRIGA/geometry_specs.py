149e005cdaf35c9c658ad94a4bcfbaac4ed6ae91
Cole Gentry <cgentry7@gmail.com>
2025-10-15 09:32:27 -0400

    Added initial framework

---
+++ progression_problems/TRIGA/geometry_specs.py
@@ -0,0 +1,9 @@
+# References
+# ----------
+#
+# [1] "University of Texas at Austin Nuclear Engineering Teaching Laboratory TRIGA Research Reactor",
+#     August 2023, https://www.nrc.gov/docs/ML2327/ML23279A146.pdf
+#
+
+CM_PER_INCH = 2.54
+
